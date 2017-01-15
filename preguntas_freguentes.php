12ed927b7cb8ee4aa4aa108bc4dbb17cd83a1a9c
Toño Castro <jcastroe1000@alumno.ipn.mx>
2017-01-15 16:40:09 -0600

    adding faqs page and style

---
+++ preguntas_freguentes.php
@@ -0,0 +1,299 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <!-- meta character set -->
+        <meta charset="utf-8">
+        <!-- Always force latest IE rendering engine or request Chrome Frame -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>SimpoWeb</title>		
+        <!-- Meta Description -->
+        <meta name="description" content="Blue One Page Creative HTML5 Template">
+        <meta name="keywords" content="one page, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+        <meta name="author" content="Muhammad Morshed">
+
+        <!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS
+        ================================================== -->
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+        <!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/slit-slider.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+        <!--Contacto Form-->
+
+        <link rel="stylesheet" href="css/form.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <!-- Modernizer Script for old Browsers -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+
+    </head>
+
+    <body id="body">
+
+        <!-- preloader -->
+        <div id="preloader">
+            <div class="loder-box">
+                <div class="battery"></div>
+            </div>
+        </div>
+        <!-- end preloader -->
+
+        <!--
+        Fixed Navigation
+        ==================================== -->
+        <header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- /responsive nav button -->
+
+                    <!-- logo -->
+                    <h1 class="navbar-brand">
+                        <a href="#body">Inicio</a>
+                    </h1>
+                    <!-- /logo -->
+                </div>
+
+                <!-- main nav -->
+                <nav class="collapse navbar-collapse navbar-right">
+                    <ul  class="nav navbar-nav">
+
+                        <li><a href="#service">Servicios</a></li>
+                        <li><a href="#portfolio">Portfolio</a></li>
+                        <li><a href="www.google.com">Contacto</a></li>
+                        <li><a href="#footer">Suscríbete</a></li>
+                        <li><a href="#footer">FAQ'S</a></li>
+                    </ul>
+                </nav>
+                <!-- /main nav -->
+
+            </div>
+        </header>
+        <!--
+        End Fixed Navigation
+        ==================================== -->
+
+        <main class="site-content" role="main">
+                    <section id="who" class="who2 section">
+            <div class="container text-center">
+                <br>
+                <h2 class="MediumItalic" style="font-size: xx-large;color: #2e2e35">PREGUNTAS FRECUENTES</h2>
+            </div>   
+            <br>
+            <br>
+            <div class="benefits text-center">
+                <div class="item  text-center">
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Hasta cuándo puedo registrar mi trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Se recibirán los trabajos hasta el día 24 de junio de 2016. 
+                                La vía de recepción de los mismos será a través de este portal y una vez que se haya llenado la solicitud de registro en www.simpowebipn.org</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> ¿Cuánto se tiene que pagar por cada trabajo registrado?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Cada trabajo registrado deberá pagar una cuota de recuperación de 
+                                $1, 468.00 (un mil cuatrocientos sesenta y ocho pesos 00/100 MN);sin embargo no se podrá hacer el depósito de dicha cantidad en la cuenta Bancomer hasta que se reciba 
+                                la carta de aceptación por correo electrónico.</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> ¿Cuándo me avisarán si mi trabajo ha sido aceptado y por qué medio?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">La semana del 8 al 12 de agosto se enviarán por correo electrónico, 
+                                las cartas de aceptación de los trabajos que hayan sido considerados para participar en el Simposium de este año.</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                           <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿En dónde se hace el depósito correspondiente a mi participación en el Simposium?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Deberá hacerse un depósito por $1, 468.00 (un mil cuatrocientos sesenta y ocho pesos 00/100 MN) 
+                                a la cuenta Bancomer 0105831016 a nombre de Fondo de Investigación Científica y Desarrollo Tecnológico del IPN. 
+                                La cantidad anteriormente indicada fue fijada atendiendo instrucciones expresas de la SHCP, por lo que agradeceremos que verifique en su baucher de depósito que se especifique precisamente esa cantidad. 
+                                Les recordamos que el original del baucher de depósito deberá ser entregado en las oficinas de ESIQIE en 
+                                la academia de integración ubicada en el primer piso, a la mayor brevedad posible después de hacer el depósito, anotando en el mismo el título del trabajo y el nombre del primer autor.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Pueden hacerme una factura?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Pueden tramitarse facturas de los depósitos que se realicen por participaciones en el Simposium, siempre y 
+                                cuando no se haga a nombre de alguna otra dependencia del politécnico. Para ello necesita llevar su Boucher a la caja de la ESIQIE ubicada en el edificio 7 primer piso con sus datos fiscales.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿En qué modalidades puedo inscribir mi trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Nuestras modalidades son Presentación oral y Cartel. Cuando se inscribe el trabajo en nuestro portal, se le preguntará cuál modalidad sugiere, 
+                                sin embargo es posible que dicha modalidad sea cambiada por sugerencia del comité editor de memorias.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Cómo puedo saber cuándo me tocará presentar mi trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">El XVII Simposium Internacional “Aportaciones de las Universidades a la Docencia, la Investigación, la Tecnología y el Desarrollo” se llevará a cabo los días 28, 29 y 30 de septiembre. Las participaciones y los horarios y lugares de las mismas se darán a conocer por correo electrónico 
+                                y también por medio de este portal en cuanto estén diseñados.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Qué tengo que hacer los días del Simposium cuando llegue a la ESIQIE?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Deberá presentarse por lo menos 15 minutos antes de la hora programada para su participación con el fin de registrarse en el vestíbulo del edificio 8 (planta baja) en la Unidad Profesional Adolfo López Mateos. 
+                                Ahí recibirá su gafete como ponente y su carpeta conmemorativa del evento.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> ¿Debo llevar algo el día que me toque presentar mi trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Si su trabajo fue aceptado en la modalidad de Cartel, tendrá que llevar su trabajo esquematizado en un póster o cartel de 90 cm x 120 cm mínimo y máximo 90cm x 200 cm.
+
+Si su trabajo fue aceptado en la modalidad de Presentación oral, tendrá que llevar su presentación de PowerPoint en la que sintetice los resultados más importantes de su trabajo.
+
+En ambas modalidades, le agradeceremos que lleve también una impresión de su Currículum resumido con el fin de que pueda ser presentado antes de su participación.                               <br>
+                                
+                                </p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Con cuánto tiempo cuento para presentar mi trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Se programan las participaciones de los trabajos en 15 minutos para cada miembro de la Mesa de Trabajo y 5 minutos para preguntas y respuestas.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">¿Tiene algún costo participar como asistente al Simposium?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">La asistencia al Simposium es libre, por lo cual le sugerimos invitar a sus colegas y alumnos.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> ¿Se pueden emitir constancias para los asistentes?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Los asistentes que quieran recibir una constancia con valor curricular deberán cubrir al menos tres jornadas comprobables con sellos de asistencia. Una jornada es considerada un turno completo, 
+                                mañana o tarde, de cada uno de los días del Simposium. Estarán a su disposición formatos de asistencia para que el participante pueda acumular los sellos de las conferencias a las que asista y así poder tramitar, con la presentación de dicho formato, su constancia.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> ¿Cuáles es el funcionamiento de las Mesas de Trabajo?</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: justify">Las funciones del presidente de mesa son:
+
+1.         Llegar 15 minutos antes del inicio de su mesa.
+
+2.         Recoger en la mesa de registro (vestíbulo del auditorio 8) el sobre rotulado con su nombre, en donde se encuentran los diplomas de los ponentes de su mesa.
+
+3.         Solicitar a los ponentes un currículo resumido para realizar su presentación.
+
+4.         Presentar al ponente y explicar a los asistentes que se dispone de 15 minutos para la presentación y cinco para preguntas y respuestas y que el tiempo deberá ser escrupulosamente respetado.
+
+5.         Entregar al final de la presentación el diploma correspondiente.
+
+6.         Cumplir las funciones del coordinador cuando sea necesario.
+                            
+                            Las funciones del coordinador de mesa son:
+
+1.         Solicitar las unidades de USB donde vienen las presentaciones.
+
+2.         Entregarlas al personal de audiovisual que se encontrará en los recintos.
+
+3.         Colaborar con el presidente de mesa.
+
+4.         Suplirlo cuando por alguna razón no se presente o tenga que ausentarse.
+
+5.         De acuerdo con el presidente de mesa, disponer  mayor tiempo de presentación o cambiar el orden cuando algún ponente no se presente.
+
+6.         Avisar al personal de organización cualquier circunstancia que deba ser atendida.
+
+7.         Comunicar al comité organizador los eventos relevantes en su mesa.
+
+ 
+
+Los ponentes:
+
+1.         Entregarán copia de su currículo resumido y la unidad de USB de su presentación 15 minutos antes del inicio de su evento con el fin de que el encargado de audiovisual organice la presentación en el orden del programa.
+
+2.         Dispondrán de 15 minutos para su presentación y cinco para preguntas y respuestas.
+
+3.         Se dirigirán a la mesa de recepción para comentar cualquier situación a resolver.</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+  
+                    
+                </div>     
+
+
+            </div>
+        </section>
+            
+
+
+
+
+
+
+
+
+        </main>
+
+       
+
+        <!-- Essential jQuery Plugins
+        ================================================== -->
+        <!-- Main jQuery -->
+        <script src="js/jquery-1.11.1.min.js"></script>
+        <!-- Twitter Bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Single Page Nav -->
+        <script src="js/jquery.singlePageNav.min.js"></script>
+        <!-- jquery.fancybox.pack -->
+        <script src="js/jquery.fancybox.pack.js"></script>
+        <!-- Google Map API -->
+        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <!-- Owl Carousel -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- jquery easing -->
+        <script src="js/jquery.easing.min.js"></script>
+        <!-- Fullscreen slider -->
+        <script src="js/jquery.slitslider.js"></script>
+        <script src="js/jquery.ba-cond.min.js"></script>
+        <!-- onscroll animation -->
+        <script src="js/wow.min.js"></script>
+        <!-- Custom Functions -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
