f63c7eedb83faf95b571d835ef071ba0101176b2
Toño Castro <jcastroe1000@alumno.ipn.mx>
2017-01-15 22:46:28 -0600

    adding people news directions

---
+++ directorio.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html lang="en" class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html lang="en" class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html lang="en" class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en" class="no-js"> <!--<![endif]-->
+    <head>
+        <!-- meta character set -->
+        <meta charset="utf-8">
+        <!-- Always force latest IE rendering engine or request Chrome Frame -->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+        <title>SimpoWeb</title>		
+        <!-- Meta Description -->
+        <meta name="description" content="Blue One Page Creative HTML5 Template">
+        <meta name="keywords" content="one page, single page, onepage, responsive, parallax, creative, business, html5, css3, css3 animation">
+        <meta name="author" content="Muhammad Morshed">
+
+        <!-- Mobile Specific Meta -->
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <!-- CSS
+        ================================================== -->
+
+        <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,700' rel='stylesheet' type='text/css'>
+
+        <!-- Fontawesome Icon font -->
+        <link rel="stylesheet" href="css/font-awesome.min.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/jquery.fancybox.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/bootstrap.min.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/owl.carousel.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/slit-slider.css">
+        <!-- bootstrap.min -->
+        <link rel="stylesheet" href="css/animate.css">
+        <!-- Main Stylesheet -->
+        <link rel="stylesheet" href="css/main.css">
+        <!--Contacto Form-->
+
+        <link rel="stylesheet" href="css/form.css">
+        <link rel="stylesheet" href="css/styles.css">
+        <link rel="stylesheet" href="css/font-face.css"> 
+         <!-- Modernizer Script for old Browsers -->
+        <script src="js/modernizr-2.6.2.min.js"></script>
+
+
+    </head>
+
+    <body id="body">
+
+        <!-- preloader -->
+        <div id="preloader">
+            <div class="loder-box">
+                <div class="battery"></div>
+            </div>
+        </div>
+        <!-- end preloader -->
+
+        <!--
+        Fixed Navigation
+        ==================================== -->
+                <header id="navigation" class="navbar-inverse navbar-fixed-top animated-header">
+            <div class="container">
+                <div class="navbar-header">
+                    <!-- responsive nav button -->
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+                        <span class="sr-only">Toggle navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+                    <!-- /responsive nav button -->
+
+                    <!-- logo -->
+                    <h1 class="navbar-brand">
+                        <a href="index.php#body">Inicio</a>
+                    </h1>
+                    <!-- /logo -->
+                </div>
+
+                <!-- main nav -->
+                <nav class="collapse navbar-collapse navbar-right" role="navigation">
+                    <ul  class="nav navbar-nav">
+
+                        <li><a href="index.php#service">Servicios</a></li>
+                        <li><a href="index.php#portafolio">Portfolio</a></li>
+                        <li><a href="index.php#contact">Contacto</a></li>
+                        <li><a href="index.php#footer">Suscríbete</a></li>
+                        <li><a href="preguntas_freguentes.php">FAQ'S</a></li>
+                    </ul>
+                </nav>
+                <!-- /main nav -->
+
+            </div>
+        </header>
+        <!--
+        End Fixed Navigation
+        ==================================== -->
+
+        <main class="site-content" role="main">
+                    <section id="who" class="who2 section">
+            <div class="container text-center">
+                <br>
+                <h2 class="MediumItalic" style="font-size: xx-large;color: #2e2e35;margin-top: 75px;">DIRECTORIO</h2>
+                <h2 class="MediumItalic" style="font-size: xx-large;color: #2e2e35;">INSTITUTO POLITÉCNICO NACIONAL</h2>
+            </div>   
+            <br>
+            <br>
+            <div class="benefits text-center">
+                <div class="item  text-center">
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Enrique Fernández Fassnacht.</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Director General</p>
+                            
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Miguel Ángel Álvarez Gómez</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35"> Secretario Academico</p>
+                            
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align:center">José Guadalupe Trujillo Ferrara</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Secretario de Investigación y Posgrado</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Primo Alberto Calva Chavarría</p>
+                            <p class="MediumItalic" style="font-size:x-large;color: #2e2e35">Director de Investigación</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Emmanuel Alejandro Merchán Cruz </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Director de Educación Superior</p>
+                        </div>   
+                        
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Emmanuel Alejandro Merchán Cruz </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Director de Educación Superior</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Dante Real Miranda </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Director de la ESIQIE</p>
+                        </div>   
+                    </div><!--l//item-inner--> 
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Victor Manuel Feregrino </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Subdirector Académico de la ESIQIE</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Juanita E. Cárdenas Rivas </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Jefe del Depto. De Ingeniería Química Industrial-ESIQIE</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Salvador Pérez Cárdenas</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Presidente Ejecutivo del Simposium</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:20px;padding-top: 1px;text-align: center">Adelina Pérez Rosas </p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Coordinadora General del Simposium</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Guillermo de la Torre Arteaga</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Sergio Flores Meza</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Anibal Quintana Mora.</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Comité de Prensa y Propaganda</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Adelina Rosas Mercado</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Mauricio Gaitán Espinosa</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Fernanda Y. Rivera Rosas</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Comité Editor de Memorias</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                     <div class="item-inner">
+                        <div >
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Lorena del Rocío Mayo Monroy</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Yahir Alejandro Martínez Nava</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Coordinación de Apoyo Logístico e Informática</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Adelina Rosas Mercado</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Coordinación General de Mesas de Trabajo</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Gerardo Ramos Serpa | Universidad “Camilo Cienfuegos”</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Cuba Manuel González Hernández  | UAEH</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Jorge Briones Chávez | UPN</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Rocío Echeverría González | UACH</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Comité Técnico</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Leticia A. Morales Sánchez</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Alma Trinidad Torres, Andrea De La Cruz Salomé</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Perla Eunice García García, Efrén Delgadillo Sánchez</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Víctor M. Guzmán Rojas, S. Farid Valladares Mendoza</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Comité de Organización</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                    <div class="item-inner">
+                        <div >
+                            
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Ana Karen Delgado Tellez</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">S. Edith Abad Castillo , Mallely E. Hernández Venavides</p>
+                            <p class="Light" style="font-size:large;color: #2e2e35;padding:10px;padding-top: 1px;text-align: center">Gustavo Eduardo Gutiérrez Puebla, Diego Víctor A. Abreu Espino</p>
+                            <p class="MediumItalic" style="font-size: x-large;color: #2e2e35">Comité de Organización</p>
+                        </div>   
+                    </div><!--l//item-inner-->
+                </div><!--l//item-inner--> 
+  
+                    
+                </div>     
+
+
+            
+        </section>
+            
+
+
+
+
+
+
+
+
+        </main>
+
+       
+
+        <!-- Essential jQuery Plugins
+        ================================================== -->
+        <!-- Main jQuery -->
+        <script src="js/jquery-1.11.1.min.js"></script>
+        <!-- Twitter Bootstrap -->
+        <script src="js/bootstrap.min.js"></script>
+        <!-- Single Page Nav -->
+        <script src="js/jquery.singlePageNav.min.js"></script>
+        <!-- jquery.fancybox.pack -->
+        <script src="js/jquery.fancybox.pack.js"></script>
+        <!-- Google Map API -->
+        <script src="http://maps.google.com/maps/api/js?sensor=false"></script>
+        <!-- Owl Carousel -->
+        <script src="js/owl.carousel.min.js"></script>
+        <!-- jquery easing -->
+        <script src="js/jquery.easing.min.js"></script>
+        <!-- Fullscreen slider -->
+        <script src="js/jquery.slitslider.js"></script>
+        <script src="js/jquery.ba-cond.min.js"></script>
+        <!-- onscroll animation -->
+        <script src="js/wow.min.js"></script>
+        <!-- Custom Functions -->
+        <script src="js/main.js"></script>
+    </body>
+</html>
