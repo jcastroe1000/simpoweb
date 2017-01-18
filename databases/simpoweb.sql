4563ebf15274f8d18625ac3883d7c5c9630e4b08
Toño Castro <jcastroe1000@alumno.ipn.mx>
2017-01-18 07:09:30 -0600

    adding new tables

--- databases/simpoweb.sql
+++ databases/simpoweb.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.5.1
+-- version 4.5.2
 -- http://www.phpmyadmin.net
 --
--- Servidor: 127.0.0.1
--- Tiempo de generación: 16-01-2017 a las 22:12:51
+-- Servidor: localhost
+-- Tiempo de generación: 18-01-2017 a las 14:07:44
 -- Versión del servidor: 10.1.19-MariaDB
--- Versión de PHP: 5.6.28
+-- Versión de PHP: 7.0.9
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -19,6 +19,75 @@ SET time_zone = "+00:00";
 --
 -- Base de datos: `simpoweb`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `administrator`
+--
+
+CREATE TABLE `administrator` (
+  `id` int(11) NOT NULL,
+  `nombre` varchar(60) NOT NULL,
+  `apellidos` varchar(60) NOT NULL,
+  `email` varchar(60) NOT NULL,
+  `password` varchar(60) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `administrator`
+--
+
+INSERT INTO `administrator` (`id`, `nombre`, `apellidos`, `email`, `password`) VALUES
+(1, 'José Antonio', 'Castro Esparza', 'administrator@gmail.com', 'Adm1n2016');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `estados_republica`
+--
+
+CREATE TABLE `estados_republica` (
+  `id` int(11) NOT NULL,
+  `estado` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Volcado de datos para la tabla `estados_republica`
+--
+
+INSERT INTO `estados_republica` (`id`, `estado`) VALUES
+(1, 'Aguascalientes'),
+(2, 'Baja California'),
+(3, 'Baja California Sur'),
+(4, 'Campeche'),
+(5, 'Coahuila'),
+(6, 'Colima'),
+(7, 'Ciudada de México'),
+(8, 'Chiapas'),
+(9, 'Chihuahua'),
+(10, 'Durango'),
+(11, 'Guadalajara'),
+(12, 'Guanajuato'),
+(13, 'Guerrero'),
+(14, 'Hidalgo'),
+(15, 'Michoacan'),
+(16, 'Morelos'),
+(17, 'Monterrey'),
+(18, 'Nayarit'),
+(19, 'Oaxaca'),
+(20, 'Puebla'),
+(21, 'Querétaro'),
+(22, 'Quintana Roo'),
+(23, 'San Luis Potosí'),
+(24, 'Sinaloa'),
+(25, 'Sonora'),
+(26, 'Tabasco'),
+(27, 'Tamaulipas'),
+(28, 'Tlaxcala'),
+(29, 'Veracruz'),
+(30, 'Yucatán'),
+(31, 'Zacatecas');
 
 -- --------------------------------------------------------
 
@@ -69,6 +138,18 @@ INSERT INTO `register` (`id`, `nombre`, `apellidos`, `email`, `telefono`, `gener
 --
 
 --
+-- Indices de la tabla `administrator`
+--
+ALTER TABLE `administrator`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `estados_republica`
+--
+ALTER TABLE `estados_republica`
+  ADD PRIMARY KEY (`id`);
+
+--
 -- Indices de la tabla `newsletter`
 --
 ALTER TABLE `newsletter`
@@ -84,6 +165,16 @@ ALTER TABLE `register`
 -- AUTO_INCREMENT de las tablas volcadas
 --
 
+--
+-- AUTO_INCREMENT de la tabla `administrator`
+--
+ALTER TABLE `administrator`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT de la tabla `estados_republica`
+--
+ALTER TABLE `estados_republica`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=32;
 --
 -- AUTO_INCREMENT de la tabla `newsletter`
 --
