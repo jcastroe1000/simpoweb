59271cf539ed65e77eec551b13d1b0ac181ab2ce
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2017-01-28 13:53:26 -0600

    adding ubication map

--- index.php
+++ index.php
@@ -528,7 +528,7 @@
                 </div>
             </section>
             <!-- end Contact section -->
-
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1880.4682578552238!2d-99.13538904205416!3d19.501366925053265!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d1f9b82ba2efb5%3A0x9f579bd1562b6eaa!2sESIQIE!5e0!3m2!1ses-419!2smx!4v1485632528035" width=100% height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
 
 
         </main>
