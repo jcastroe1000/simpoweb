3d89ed9d0126f61f345aa51e5a0e15b02700753d
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2017-05-28 19:50:40 -0500

    adding connections

--- config.php
+++ config.php
@@ -31,4 +31,28 @@
     if ($mysqli8->connect_errno) {
         echo "Failed to connect to MySQL: " . $mysqli8->connect_error;
     }
+    $mysqli9 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli9->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli9->connect_error;
+    }
+    $mysqli10 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli10->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli10->connect_error;
+    }
+    $mysqli11 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli11->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli11->connect_error;
+    }
+    $mysqli12 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli12->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli12->connect_error;
+    }
+    $mysqli13 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli13->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli13->connect_error;
+    }
+    $mysqli14 = new mysqli("127.0.0.1", "root", "", "simpoweb");
+    if ($mysqli14->connect_errno) {
+        echo "Failed to connect to MySQL: " . $mysqli14->connect_error;
+    }
 ?>
