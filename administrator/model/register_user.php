8be518687729601f43a9f77e116fc52d3d8f54aa
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2017-12-30 22:34:31 -0600

    addin new structur files

---
+++ administrator/model/register_user.php
@@ -0,0 +1,44 @@
+<?php
+
+header('Content-type: application/json');
+include "../config.php";
+error_reporting(E_ALL);
+
+
+$email_contact = $_GET['email_contact'];
+
+$query = "SELECT email FROM user_register WHERE email='$email_contact' ";
+$result = mysqli_query($mysqli, $query)or die(mysqli_error());
+$num_row = mysqli_num_rows($result);
+$row = mysqli_fetch_array($result);
+if ($email_contact == $row['email']) {
+    $response_array['status'] = 'error';
+
+    echo json_encode($response_array);
+} else {
+    
+
+    $user__name = $_GET['name'];
+$surnames = $_GET['surnames'];
+$email_contact = $_GET['email_contact'];
+$telephone_contact = $_GET['telephone_contact'];
+$password = $_GET['password'];
+$hash_pass= crypt($password,'$1$somethin$');
+$date_registration = $_GET['creation_date'];
+$gender = $_GET['gender'];
+
+require_once '../model/user_register.php';
+require_once '../model/user_register_admin.php';
+
+
+$query1 = "INSERT INTO user_register(name,surnames,telephone, gender,email,password,creation_date) "
+        . "VALUES ('$user__name', '$surnames', '$telephone_contact', '$gender', '$email_contact', '$hash_pass','$date_registration')";
+mysqli_query($mysqli, $query1);
+
+    $response_array['status'] = 'success';
+    echo json_encode($response_array);
+//Cerramos la conexion
+    $mysqli->close();
+}
+
+?>
--- administrator/production/registrar_curso.php
+++ administrator/production/registrar_curso.php
@@ -21,6 +21,7 @@ $user_name = utf8_encode($_SESSION['user_name']);
 
         <!-- Bootstrap -->
         <link href="../vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+       
         <!-- Font Awesome -->
         <link href="../vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
         <!-- NProgress -->
