6789f977b31b7efdddc66a07fd01653078e8212d
jcastroe1000 <jcastroe1000@alumno.ipn.mx>
2018-03-19 12:23:27 -0600

    adding menu toogle

--- administrator/consultar/cursos.php
+++ administrator/consultar/cursos.php
@@ -150,7 +150,9 @@ $user_name = $_SESSION['user_name'];
                     <div class="nav_menu">
                         <nav>
 
-
+                            <div class="nav toggle">
+                                <a id="menu_toggle"><i class="fa fa-bars"></i></a>
+                            </div>
                             <ul class="nav navbar-nav navbar-right">
                                 <li class="">
                                     <a href="javascript:;" class="user-profile dropdown-toggle" data-toggle="dropdown" aria-expanded="false">
