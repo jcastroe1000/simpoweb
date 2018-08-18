1c18fd95159fb83ee8cfcb37079f229b284b5dc8
jcastroe1000 <jcastroe1000@alumno.com.mx>
2018-08-18 17:02:51 -0500

    adding new attribute places_available

--- administrator/crear/registrar_diplomado.php
+++ administrator/crear/registrar_diplomado.php
@@ -356,10 +356,15 @@ $user_name = utf8_encode($_SESSION['user_name']);
                                     <textarea name="aditional_information" id="aditional_information" placeholder="Dirigido..." style="height: 123px;width: 100%"
                                               class="f1-about-yourself form-control3" id="f1-about-yourself"></textarea>
                                 </div>
-                                <div class="form-group">
+                                <div class="form-group" style="width: 45%;display: inline-block;">
                                     <label class="etiquetas" for="f1-first-name">Costo:</label>
                                     <input type="number" name="cost" id="cost" min="1" max="1000000"  class="f1-first-name form-control3" 
                                            id="f1-first-name" style="width: 100%" placeholder="Ingresa un monto">
+                                </div>
+                                <div class="form-group" style="width: 45%;display: inline-block;float: right">
+                                    <label class="etiquetas" for="f1-first-name">Lugares Disponibles:</label>
+                                    <input type="number" name="places_available" id="places_available" placeholder="Lugares Disponibles" 
+                                           class="f1-first-name form-control3" id="f1-first-name" style="width: 100%">
                                 </div>
                                 <div class="form-group">
                                     <label class="etiquetas" for="f1-first-name">Método de Pago:</label>
