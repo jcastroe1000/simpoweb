9d85e8b012964397fd112c5800f3c3956dd44296
jcastroe1000 <jcastroe1000@alumno.com.mx>
2018-09-22 18:50:51 -0500

    adding new model to save matter

---
+++ administrator/js/update/update_matter.js
@@ -0,0 +1,46 @@
+$(function() {
+  $('#update_faq_course').submit(function() {
+    var comprobar = $('#question').val().length * $('#answer').val().length 
+                * $('#id_faqs').val().length;
+    if (comprobar > 0) {
+      
+          var formulario = $('#update_faq_course');
+          var datos = formulario.serialize();
+          var archivos = new FormData();
+          var url = '../update/update_faqs.php';
+          
+          $.ajax({
+            url: url + '?' + datos,
+            type: 'POST',
+            contentType: false,
+            data: archivos,
+            processData: false,
+            beforeSend: function() {
+                
+                    $('#myModal').modal('show');
+             
+            },
+            success: function (data) {
+                
+                setTimeout(function () {
+                  $('#cargando ').modal('show');
+                    
+                }, 9500);
+                setTimeout(function () {
+                    
+                    $(location).attr('href', '../consultar/preguntas_cursos.php');
+                }, 12000);
+            },
+            error: function(data) {
+                $('#cargando h3').text('Ocurrio un error. Intentalo de nuevo.');
+            }
+          });
+          return false;
+      
+    } else {
+      $('#cargando').modal('show');
+      $('#cargando h3').text("Llena todos los campos");
+      return false;
+    }
+  })
+})
